2fb394ad79e9e648db0febb1429b9191b1cc99b8
Mike Jolley <mike.jolley@me.com>
2013-12-17 11:57:35 +0000

    Filetype icons are needed for frontend

---
+++ templates/pagination.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Pagination - Show numbered pagination.
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $dlm_max_num_pages;
+
+if ( $dlm_max_num_pages <= 1 )
+	return;
+?>
+<nav class="download-monitor-pagination">
+	<?php
+		echo paginate_links( apply_filters( 'download_monitor_pagination_args', array(
+			'base' 			=> str_replace( 999999999, '%#%', get_pagenum_link( 999999999 ) ),
+			'format' 		=> '',
+			'current' 		=> max( 1, get_query_var('paged') ),
+			'total' 		=> $dlm_max_num_pages,
+			'prev_text' 	=> '&larr;',
+			'next_text' 	=> '&rarr;',
+			'type'			=> 'list',
+			'end_size'		=> 3,
+			'mid_size'		=> 3
+		) ) );
+	?>
+</nav>
