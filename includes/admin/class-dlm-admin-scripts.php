4a4a487015d5062bf16cc41155c6067cfb4f2d5e
Barry Kooij <git@barrykooij.nl>
2015-01-02 22:08:50 +0100

    Added admin script class. Related #209

---
+++ includes/admin/class-dlm-admin-scripts.php
@@ -0,0 +1,59 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+class DLM_Admin_Scripts {
+
+	/**
+	 * Setup hooks
+	 */
+	public function setup() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * Enqueue admin scripts
+	 */
+	public function enqueue_scripts() {
+		global $pagenow;
+
+		if ( $pagenow == 'post.php' || $pagenow == 'post-new.php' ) {
+
+			// Load PL JS
+			wp_enqueue_script(
+				'dlm_edit_post',
+				plugins_url( '/assets/js/edit-post' . ( ( ! SCRIPT_DEBUG ) ? '.min' : '' ) . '.js', WP_DLM::get_plugin_file() ),
+				array( 'jquery' ),
+				DLM_VERSION
+			);
+
+			// Make JavaScript strings translatable
+			wp_localize_script( 'dlm_edit_post', 'dlm_strings', $this->get_strings( 'edit-post' ) );
+
+		}
+	}
+
+	/**
+	 * Get JS strings
+	 *
+	 * @param $file
+	 *
+	 * @return array
+	 */
+	private function get_strings( $file ) {
+		switch ( $file ) {
+			case 'edit-post':
+				$strings = array(
+					'insert_download' => __( 'Insert Download', 'download-monitor' )
+				);
+				break;
+			default:
+				$strings = array();
+		}
+
+		return $strings;
+	}
+
+}
