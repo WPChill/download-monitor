072a55c6c32d795d51465a91ae82f18b0b35b311
Matt Mower <mowerm@gmail.com>
2015-07-09 15:23:16 -0500

    Add IP-in-range check to Utils class

--- includes/class-dlm-utils.php
+++ includes/class-dlm-utils.php
@@ -27,6 +27,27 @@ abstract class DLM_Utils {
 	}
 
 	/**
+	 * Check if a given ip is in a network (IPv4 only)
+	 * https://gist.github.com/tott/7684443
+	 *
+	 * @param  string $ip    IP to check in IPv4 format eg. 127.0.0.1
+	 * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+	 * @return boolean true if the ip is in this range / false if not.
+	 */
+	public static function ip_in_range( $ip, $range ) {
+		if ( strpos( $range, '/' ) == false ) {
+			$range .= '/32';
+		}
+		// $range is in IP/CIDR format e.g. 127.0.0.1/24
+		list( $range, $netmask ) = explode( '/', $range, 2 );
+		$range_decimal = ip2long( $range );
+		$ip_decimal = ip2long( $ip );
+		$wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+		$netmask_decimal = ~ $wildcard_decimal;
+		return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+	}
+
+	/**
 	 * Local independent basename
 	 *
 	 * @param string $filepath
