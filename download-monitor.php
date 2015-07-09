9856c45984882e8ddb02dfdf0deb535879ca52a4
Barry Kooij <git@barrykooij.nl>
2015-07-09 18:21:05 +0200

    Added credit note for Mike

--- download-monitor.php
+++ download-monitor.php
@@ -23,6 +23,8 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Original project created by Mike Jolley.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
