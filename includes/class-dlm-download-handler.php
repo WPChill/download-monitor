dc6b4e20f5d22b95d9b31eeb4c56cd94f90724ed
Matt Mower <mowerm@gmail.com>
2015-07-10 12:15:28 -0500

    Accept regex patterns for user agent blacklist

--- includes/class-dlm-download-handler.php
+++ includes/class-dlm-download-handler.php
@@ -149,9 +149,16 @@ class DLM_Download_Handler {
 
 						if ( ! empty( $blacklisted_ua ) ) {
 							// check if blacklisted user agent is found in request user agent
-							if ( false !== stristr( $visitor_ua, $blacklisted_ua ) ) {
-								$can_download = false;
-								break;
+							if ( '/' == $blacklisted_ua[0] && '/' == substr( $blacklisted_ua, -1 ) ) { // /regex/ pattern
+								if ( preg_match( $blacklisted_ua, $visitor_ua ) ) {
+									$can_download = false;
+									break;
+								}
+							} else { // string matching
+								if ( false !== stristr( $visitor_ua, $blacklisted_ua ) ) {
+									$can_download = false;
+									break;
+								}
 							}
 						}
 
