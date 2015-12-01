83c22d6d03e34d5a923dba4efa125d7dd6e810dc
Barry Kooij <git@barrykooij.nl>
2015-12-01 15:06:27 +0100

    Added some extra checks to prevent stristr empty needle notices

--- includes/class-dlm-download-handler.php
+++ includes/class-dlm-download-handler.php
@@ -142,14 +142,21 @@ class DLM_Download_Handler {
 				// get blacklisted user agents
 				$blacklisted_uas = preg_split( "/\r?\n/", trim( get_option( 'dlm_user_agent_blacklist', "" ) ) );
 
-				// loop through blacklisted user agents
-				foreach ( $blacklisted_uas as $blacklisted_ua ) {
+				if ( ! empty( $blacklisted_uas ) ) {
 
-					// check if blacklisted user agent is found in request user agent
-					if ( false !== stristr( $visitor_ua, $blacklisted_ua ) ) {
-						$can_download = false;
-						break;
+					// loop through blacklisted user agents
+					foreach ( $blacklisted_uas as $blacklisted_ua ) {
+
+						if ( ! empty( $blacklisted_ua ) ) {
+							// check if blacklisted user agent is found in request user agent
+							if ( false !== stristr( $visitor_ua, $blacklisted_ua ) ) {
+								$can_download = false;
+								break;
+							}
+						}
+
 					}
+
 				}
 
 			}
