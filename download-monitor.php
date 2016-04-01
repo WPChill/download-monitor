da5832b507d7bd5e75bd6c6cdad2106869f36ce6
Barry Kooij <b.kooij@gmail.com>
2016-04-01 16:25:10 +0200

    dev version bump

--- download-monitor.php
+++ download-monitor.php
@@ -3,7 +3,7 @@
 	Plugin Name: Download Monitor
 	Plugin URI: https://www.download-monitor.com
 	Description: A full solution for managing downloadable files, monitoring downloads and outputting download links and file information on your WordPress powered site.
-	Version: 1.9.2
+	Version: 1.9.3-dev
 	Author: Never5
 	Author URI: https://www.never5.com
 	Requires at least: 3.8
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 } // Exit if accessed directly
 
 // Define DLM Version
-define( 'DLM_VERSION', '1.9.2' );
+define( 'DLM_VERSION', '1.9.3-dev' );
 
 function __download_monitor_main() {
 
