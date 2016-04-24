1aa19ebea1bca6f33ee4956ed21eedbb99bfc248
Peter Prins <peterprins@users.noreply.github.com>
2016-04-24 04:09:26 -0700

    Update class-dlm-utils.php
    Fix the 0.0.0.0 IP Address being reported in Redirection Log and 404s

--- includes/class-dlm-utils.php
+++ includes/class-dlm-utils.php
@@ -8,7 +8,7 @@ abstract class DLM_Utils {
 	 * @return string
 	 */
 	public static function get_visitor_ip() {
-		return sanitize_text_field( ! empty( $_SERVER['HTTP_X_FORWARD_FOR'] ) ? $_SERVER['HTTP_X_FORWARD_FOR'] : $_SERVER['REMOTE_ADDR'] );
+		return sanitize_text_field( ( ! empty( $_SERVER['HTTP_X_FORWARD_FOR'] ) != '0.0.0.0' ) ? $_SERVER['HTTP_X_FORWARD_FOR'] : $_SERVER['REMOTE_ADDR'] );
 	}
 
 	/**
