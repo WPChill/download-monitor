244bb6d1fac588c172040fbcd6fb359fd56a26f6
Barry Kooij <b.kooij@gmail.com>
2017-02-23 11:58:24 +0100

    Listen to hash switches, update tabs when hash does change

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,35 +1,45 @@
 jQuery( function ( $ ) {
 
-    $( '.nav-tab-wrapper a' ).click( function () {
-        $( '.settings_panel' ).hide();
-        $( '.nav-tab-active' ).removeClass( 'nav-tab-active' );
-        $( $( this ).attr( 'href' ) ).show();
-        $( this ).addClass( 'nav-tab-active' );
-        return true;
-    } );
+	$( '.nav-tab-wrapper a' ).click( function () {
 
-    $( '#setting-dlm_default_template' ).change( function () {
-        if ( $( this ).val() == 'custom' ) {
-            $( '#setting-dlm_custom_template' ).closest( 'tr' ).show();
-        } else {
-            $( '#setting-dlm_custom_template' ).closest( 'tr' ).hide();
-        }
-    } ).change();
+		if ( $( this ).hasClass( 'nav-tab-active' ) ) {
+			return false;
+		}
 
-    $( '#setting-dlm_enable_logging' ).change( function () {
-        if ( $( this ).is( ":checked" ) === true ) {
-            $( '#setting-dlm_count_unique_ips' ).closest( 'tr' ).show();
-        } else {
-            $( '#setting-dlm_count_unique_ips' ).closest( 'tr' ).hide();
-        }
-    } ).change();
+		$( '.settings_panel' ).hide();
+		$( '.nav-tab-active' ).removeClass( 'nav-tab-active' );
+		$( $( this ).attr( 'href' ) ).show();
+		$( this ).addClass( 'nav-tab-active' );
+		return true;
+	} );
 
-    // load tab of hash, if no hash is present load first tab.
-    if ( window.location.hash ) {
-        var active_tab = window.location.hash.replace( '#', '' );
-        $( '.nav-tab-wrapper a#dlm-tab-' + active_tab ).click();
-    } else {
-        $( '.nav-tab-wrapper a:first' ).click();
-    }
+	$( '#setting-dlm_default_template' ).change( function () {
+		if ( $( this ).val() == 'custom' ) {
+			$( '#setting-dlm_custom_template' ).closest( 'tr' ).show();
+		} else {
+			$( '#setting-dlm_custom_template' ).closest( 'tr' ).hide();
+		}
+	} ).change();
+
+	$( '#setting-dlm_enable_logging' ).change( function () {
+		if ( $( this ).is( ":checked" ) === true ) {
+			$( '#setting-dlm_count_unique_ips' ).closest( 'tr' ).show();
+		} else {
+			$( '#setting-dlm_count_unique_ips' ).closest( 'tr' ).hide();
+		}
+	} ).change();
+
+	// load tab of hash, if no hash is present load first tab.
+	if ( window.location.hash ) {
+		var active_tab = window.location.hash.replace( '#', '' );
+		$( '.nav-tab-wrapper a#dlm-tab-' + active_tab ).click();
+	} else {
+		$( '.nav-tab-wrapper a:first' ).click();
+	}
+
+	$( window ).bind( 'hashchange', function ( e ) {
+		var active_tab = window.location.hash.replace( '#', '' );
+		$( '.nav-tab-wrapper a#dlm-tab-' + active_tab ).click();
+	} );
 
 } );
