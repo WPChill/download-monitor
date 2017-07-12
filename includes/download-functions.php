6142f4051f523a0582f411717930d0ef96c4b306
Kurt Zenisek <KZeni1@gmail.com>
2017-07-12 13:47:05 -0500

    Make it so query_var uses a template which then redirects to the download

--- includes/download-functions.php
+++ includes/download-functions.php
@@ -13,3 +13,15 @@ function dlm_get_default_download_template() {
 
 	return $default;
 }
+
+// Filter the single_template with our custom function
+function dlm_download_single_post_type_template($single) {
+    global $wp_query, $post;
+    /* Checks for single template by post type */
+    if ($post->post_type == 'dlm_download' || $post->post_type == 'dlm_download_version'){
+        if(file_exists(plugin_dir_path(DLM_PLUGIN_FILE) . 'includes/download-query-var-redirect.php'))
+            return plugin_dir_path(DLM_PLUGIN_FILE) . 'includes/download-query-var-redirect.php';
+    }
+    return $single;
+}
+add_filter('single_template', 'dlm_download_single_post_type_template');
