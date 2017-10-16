d694c18bc86a6a89c61ee5054e423e2799c2a42f
Barry Kooij <b.kooij@gmail.com>
2017-10-16 17:13:26 +0200

    WP Download Repo WIP

--- includes/Download/WordPressDownloadRepository.php
+++ includes/Download/WordPressDownloadRepository.php
@@ -43,9 +43,60 @@ class DLM_WordPress_Download_Repository implements DLM_Download_Repository {
 	/**
 	 * @param DLM_Download $download
 	 *
+	 * @throws \Exception
+	 *
 	 * @return bool
 	 */
 	public function persist( $download ) {
+
+		// check if new download or existing
+		if ( 0 == $download->get_id() ) {
+
+			// create
+			$download_id = wp_insert_post( array(
+				'post_title'   => $download->get_title(),
+				'post_content' => $download->get_description(),
+				'post_excerpt' => $download->get_excerpt(),
+				'post_author'  => $download->get_author(),
+				'post_type'    => 'dlm_download',
+				'post_status'  => $download->get_status()
+			) );
+
+			if ( is_wp_error( $download_id ) ) {
+				throw new \Exception( 'Unable to insert download in WordPress database' );
+			}
+			// set new vehicle ID
+			$download->set_id( $download_id );
+
+		} else {
+
+			// update
+			$download_id = wp_update_post( array(
+				'ID'           => $download->get_id(),
+				'post_title'   => $download->get_title(),
+				'post_content' => $download->get_description(),
+				'post_excerpt' => $download->get_excerpt(),
+				'post_author'  => $download->get_author(),
+				'post_status'  => $download->get_status()
+			) );
+
+			if ( is_wp_error( $download_id ) ) {
+				throw new \Exception( 'Unable to update download in WordPress database' );
+			}
+
+		}
+
+		// persist 'Download Options'
+		update_post_meta( $download_id, '_featured', ( ( $download->is_featured() ) ? 'yes' : 'no' ) );
+		update_post_meta( $download_id, '_members_only', ( ( $download->is_members_only() ) ? 'yes' : 'no' ) );
+		update_post_meta( $download_id, '_redirect_only', ( ( $download->is_redirect_only() ) ? 'yes' : 'no' ) );
+
+		// other download meta
+		update_post_meta( $download_id, '_download_count', $download->get_download_count() );
+
+		// clear versions transient
+		$download->clear_versions_transient();
+
 		return true;
 	}
 
