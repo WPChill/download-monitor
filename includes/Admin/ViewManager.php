39df21043a91815306c6409ea678419302811d7b
Barry Kooij <b.kooij@gmail.com>
2017-10-18 14:17:42 +0200

    Introduced new ViewManager for loading admin views

---
+++ includes/Admin/ViewManager.php
@@ -0,0 +1,35 @@
+<?php
+
+class DLM_View_Manager {
+
+	/**
+	 * Display a view
+	 *
+	 * @param String $view
+	 * @param array $vars
+	 * @param String $path
+	 */
+	public function display( $view, $vars=array(), $path = '' ) {
+
+		// setup variables
+		extract( $vars );
+
+		// set default path if $path is empty
+		if ( empty( $path ) ) {
+			$path = download_monitor()->get_plugin_path() . 'assets/views/';
+		}
+
+		// setup full view path
+		$view = $path . $view . '.php';
+
+		error_log($view, 0 );
+
+		// check if view exists
+		if ( file_exists( $view ) ) {
+
+			// load view
+			include( $view );
+		}
+	}
+
+}
