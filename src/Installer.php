a3549b40a15d97855cfe051ae758ae0472effefb
Barry Kooij <b.kooij@gmail.com>
2017-11-03 11:47:29 +0100

    Modified Installer DB creation SQL to install correct new colls

--- src/Installer.php
+++ src/Installer.php
@@ -100,16 +100,16 @@ class DLM_Installer {
 		$dlm_tables = "
 	CREATE TABLE `" . $wpdb->prefix . "download_log` (
 	  ID bigint(20) NOT NULL auto_increment,
-	  type varchar(200) NOT NULL default 'download',
 	  user_id bigint(20) NOT NULL,
 	  user_ip varchar(200) NOT NULL,
 	  user_agent varchar(200) NOT NULL,
 	  download_id bigint(20) NOT NULL,
 	  version_id bigint(20) NOT NULL,
 	  version varchar(200) NOT NULL,
-	  download_date datetime NOT NULL default '0000-00-00 00:00:00',
-	  download_status varchar(200) NULL,
-	  download_status_message varchar(200) NULL,
+	  download_date datetime DEFAULT NULL,
+	  download_status varchar(200) DEFAULT NULL,
+	  download_status_message varchar(200) DEFAULT NULL,
+	  meta_data longtext DEFAULT NULL,
 	  PRIMARY KEY  (ID),
 	  KEY attribute_name (download_id)
 	) $collate;
