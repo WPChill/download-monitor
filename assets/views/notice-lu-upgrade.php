d1db365df7d6039eeaabaf49dbbd69e627ef9c1b
Barry Kooij <b.kooij@gmail.com>
2017-12-19 13:47:43 +0100

    Made upgrade message text translatable

--- assets/views/notice-lu-upgrade.php
+++ assets/views/notice-lu-upgrade.php
@@ -4,9 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 } // Exit if accessed directly
 ?>
 <div class="dlm-lu-upgrade-notice">
-	<h3>It looks like you upgraded to the latest version of Download Monitor from a legacy version (3.x)</h3>
-	<p>Currently your downloads don't work like they should, we need to <strong>upgrade your downloads</strong> before they'll work again.</p>
-	<p>We've created an upgrading tool that will do all the work for you. You can read more about this tool on <a href="https://www.download-monitor.com/kb/legacy-upgrade?utm_source=plugin&utm_medium=dlm-lu-upgrade-notice&utm_campaign=dlm-lu-more-information" target="_blank">our website (click here)</a> or start the upgrade now.</p>
-    <a href="<?php echo admin_url( 'options.php?page=dlm_legacy_upgrade' ); ?>" class="button">Take me to the Upgrade Tool</a>
-    <a href="<?php echo admin_url( 'edit.php?post_type=dlm_download&dlm_lu_hide_notice=1' ); ?>" class="dlm-lu-upgrade-notice-hide">hide notice</a>
+	<h3><?php _e('It looks like you upgraded to the latest version of Download Monitor from a legacy version (3.x)', 'download-monitor' ); ?></h3>
+	<p><?php printf( __( "Currently your downloads don't work like they should, we need to %s before they'll work again.", 'download-monitor' ), sprintf( '<strong>%s</strong>', __( 'upgrade your downloads', 'download-monitor' ) ) ); ?></p>
+	<p><?php printf( __( "We've created an upgrading tool that will do all the work for you. You can read more about this tool on %sour website (click here)%s or start the upgrade now.", 'download-monitor'), '<a href="https://www.download-monitor.com/kb/legacy-upgrade?utm_source=plugin&utm_medium=dlm-lu-upgrade-notice&utm_campaign=dlm-lu-more-information" target="_blank">', '</a>' ); ?></p>
+    <a href="<?php echo admin_url( 'options.php?page=dlm_legacy_upgrade' ); ?>" class="button"><?php _e( 'Take me to the Upgrade Tool', 'download-monitor' ); ?></a>
+    <a href="<?php echo admin_url( 'edit.php?post_type=dlm_download&dlm_lu_hide_notice=1' ); ?>" class="dlm-lu-upgrade-notice-hide"><?php _e( 'hide notice', 'download-monitor' ); ?></a>
 </div>
