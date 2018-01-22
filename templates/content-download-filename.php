0fcdaf9fad1539a84ec4525068aa5bc8ae460573
Kurt Zenisek <KZeni1@gmail.com>
2018-01-22 16:11:02 -0600

    Make sure get_version() is a valid function before trying to use it.

--- templates/content-download-filename.php
+++ templates/content-download-filename.php
@@ -9,10 +9,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** @var DLM_Download $dlm_download */
 ?>
-<a class="download-link filetype-icon <?php echo 'filetype-' . $dlm_download->get_version()->get_filetype(); ?>"
-   title="<?php if ( $dlm_download->get_version()->has_version_number() ) {
-	   printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
-   } ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php echo $dlm_download->get_version()->get_filename(); ?>
+<a class="download-link" title="<?php if ( $dlm_download->get_version() && $dlm_download->get_version()->has_version_number() ) {
+	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
+} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+	<?php $dlm_download->the_title(); ?>
 	(<?php printf( _n( '1 download', '%d downloads', $dlm_download->get_download_count(), 'download-monitor' ), $dlm_download->get_download_count() ) ?>)
 </a>
