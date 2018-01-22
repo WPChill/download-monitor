67829b4ee7e6d61c0465c61defb57bff5a929b84
Kurt Zenisek <KZeni1@gmail.com>
2018-01-22 16:09:54 -0600

    Prevent empty line-breaks when just outputting the title.

--- templates/content-download-title.php
+++ templates/content-download-title.php
@@ -8,9 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 } // Exit if accessed directly
 
 /** @var DLM_Download $dlm_download */
-?>
-<a class="download-link" title="<?php if (  $dlm_download->get_version() && $dlm_download->get_version()->has_version_number() ) {
+?><a class="download-link" title="<?php if (  $dlm_download->get_version() && $dlm_download->get_version()->has_version_number() ) {
 	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
-} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php $dlm_download->the_title(); ?>
-</a>
+} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow"><?php $dlm_download->the_title(); ?></a>
