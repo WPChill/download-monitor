e161fc52855499389a5c9bb71f54c34c3f9dc7ea
Kurt Zenisek <KZeni1@gmail.com>
2018-01-22 16:17:15 -0600

    Make sure get_version() is a valid function before trying to use it.

--- templates/content-download-filename.php
+++ templates/content-download-filename.php
@@ -9,9 +9,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** @var DLM_Download $dlm_download */
 ?>
-<a class="download-link" title="<?php if ( $dlm_download->get_version() && $dlm_download->get_version()->has_version_number() ) {
-	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
-} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php $dlm_download->the_title(); ?>
+<a class="download-link filetype-icon <?php if( $dlm_download->get_version() ) { echo 'filetype-' . $dlm_download->get_version()->get_filetype(); } ?>"
+   title="<?php if ( $dlm_download->get_version() && $dlm_download->get_version()->has_version_number() ) {
+	   printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
+   } ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+	<?php if( $dlm_download->get_version() ): ?>
+	<?php echo $dlm_download->get_version()->get_filename(); ?>
+	<?php else: ?>
+	<small><?php echo $dlm_download->the_title(); ?></small>
+	<?php endif; ?>
 	(<?php printf( _n( '1 download', '%d downloads', $dlm_download->get_download_count(), 'download-monitor' ), $dlm_download->get_download_count() ) ?>)
 </a>
