b260aae34be253eb9c045112d6b06c12d2584cc1
Barry Kooij <b.kooij@gmail.com>
2018-01-24 21:59:19 +0100

    Added an extra check to if website needs upgrading. If there are new downloads in the database, no upgrade is recommended

--- src/LegacyUpgrader/Checker.php
+++ src/LegacyUpgrader/Checker.php
@@ -19,15 +19,27 @@ class DLM_LU_Checker {
 	private function has_legacy_tables() {
 		global $wpdb;
 
-		$du            = new DLM_LU_Download_Upgrader();
-		$legacy_tables = $du->get_legacy_tables();
-		$sql           = "SELECT 1 FROM `" . $legacy_tables['files'] . "` LIMIT 1;";
-		$o_suppress_errors = $wpdb->suppress_errors;
+		$du                    = new DLM_LU_Download_Upgrader();
+		$legacy_tables         = $du->get_legacy_tables();
+		$sql                   = "SELECT 1 FROM `" . $legacy_tables['files'] . "` LIMIT 1;";
+		$o_suppress_errors     = $wpdb->suppress_errors;
 		$wpdb->suppress_errors = true;
-		$r             = $wpdb->query( $sql );
+		$r                     = $wpdb->query( $sql );
 		$wpdb->suppress_errors = $o_suppress_errors;
 
 		return ( $r !== false );
+	}
+
+	/**
+	 * Returns true if there is at least one 'new' downloads.
+	 * A new download is a custom post type with type 'dlm_download'
+	 * @return bool
+	 */
+	private function has_modern_downloads() {
+		$repo   = new DLM_WordPress_Download_Repository();
+		$amount = $repo->num_rows();
+
+		return ( $amount > 0 );
 	}
 
 	/**
@@ -51,9 +63,21 @@ class DLM_LU_Checker {
 			return false;
 		}
 
+		// check if we already upgraded
 		if ( ! $this->has_been_upgraded() ) {
+
+			// check if we have legacy tables
 			if ( $this->has_legacy_tables() ) {
-				return true;
+
+				/**
+				 * Check if there are already 'new' download
+				 * We're doing this because there are users that manually upgraded in the past
+				 * So they will have the legacy tables but don't need converting
+				 */
+				if ( ! $this->has_modern_downloads() ) {
+					return true;
+				}
+
 			}
 		}
 
