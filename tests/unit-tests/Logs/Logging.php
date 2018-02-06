e5b0967032b740fa988eb786020225880f9738a6
Barry Kooij <b.kooij@gmail.com>
2018-02-06 19:50:13 +0100

    Added Logging tests start, test_has_ip_downloaded_version() as first test

---
+++ tests/unit-tests/Logs/Logging.php
@@ -0,0 +1,47 @@
+<?php
+
+class DLM_Test_Logging extends DLM_Unit_Test_Case {
+
+	/**
+	 * tearDown
+	 */
+	public function tearDown() {
+		global $wpdb;
+		parent::tearDown();
+		DLM_Test_WP_DB_Helper::truncate( $wpdb->download_log );
+	}
+
+	public function test_has_ip_downloaded_version() {
+
+		// repo
+		$wp_repo = new DLM_WordPress_Log_Item_Repository();
+
+		// get ip address
+		$ip = DLM_Utils::get_visitor_ip();
+
+		// logging object
+		$logging = new DLM_Logging();
+
+		// create new version
+		$version = new DLM_Download_Version();
+		$version->set_id( 1 );
+
+		// create log item
+		$dummy_log = new DLM_Log_Item();
+		$dummy_log->set_user_id( 0 );
+		$dummy_log->set_user_ip( $ip );
+		$dummy_log->set_version_id( 1 );
+		$dummy_log->set_user_agent( 'test' );
+		$dummy_log->set_version( '1.0' );
+
+		// check on empty database
+		$this->assertEquals( false, $logging->has_ip_downloaded_version( $version ) );
+
+		// store log item
+		$wp_repo->persist( $dummy_log );
+
+		// now we should have already downloaded the file
+		$this->assertEquals( true, $logging->has_ip_downloaded_version( $version ) );
+
+	}
+}
