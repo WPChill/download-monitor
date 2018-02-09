30e88e7f534466c296d1e33fc53dd8ac33f88740
Barry Kooij <b.kooij@gmail.com>
2018-02-09 13:36:47 +0100

    Cache we need to upgrade legacy downloads. This prevents us from checking if we need to upgrade on every pageload. Fixes #475

--- src/LegacyUpgrader/Checker.php
+++ src/LegacyUpgrader/Checker.php
@@ -19,6 +19,8 @@ class DLM_LU_Checker {
 	private function has_legacy_tables() {
 		global $wpdb;
 
+		error_log( 'checking legacy tables', 0 );
+
 		$du                    = new DLM_LU_Download_Upgrader();
 		$legacy_tables         = $du->get_legacy_tables();
 		$sql                   = "SELECT 1 FROM `" . $legacy_tables['files'] . "` LIMIT 1;";
@@ -49,6 +51,7 @@ class DLM_LU_Checker {
 	 */
 	public function mark_upgraded() {
 		update_option( DLM_Constants::LU_OPTION_UPGRADED, 1 );
+		update_option( DLM_Constants::LU_OPTION_NEEDS_UPGRADING, 0 );
 	}
 
 	/**
@@ -63,25 +66,45 @@ class DLM_LU_Checker {
 			return false;
 		}
 
-		// check if we already upgraded
-		if ( ! $this->has_been_upgraded() ) {
+		/**
+		 * Check if we already checked if need to upgrade.
+		 * If we already checked we stored the result in this option.
+		 * This prevents us from checking on every admin load.
+		 */
+		$needs_upgrading = get_option( DLM_Constants::LU_OPTION_NEEDS_UPGRADING, null );
 
-			// check if we have legacy tables
-			if ( $this->has_legacy_tables() ) {
+		// if the option is null, it's not set yet and we need to check.
+		if ( null === $needs_upgrading ) {
 
-				/**
-				 * Check if there are already 'new' download
-				 * We're doing this because there are users that manually upgraded in the past
-				 * So they will have the legacy tables but don't need converting
-				 */
-				if ( ! $this->has_modern_downloads() ) {
-					return true;
+			// set default to 0 (no)
+			$needs_upgrading = 0;
+
+			// check if we already upgraded
+			if ( ! $this->has_been_upgraded() ) {
+
+				// check if we have legacy tables
+				if ( $this->has_legacy_tables() ) {
+
+					/**
+					 * Check if there are already 'new' download
+					 * We're doing this because there are users that manually upgraded in the past
+					 * So they will have the legacy tables but don't need converting
+					 */
+					if ( ! $this->has_modern_downloads() ) {
+
+						// this site needs upgrading, set to 1 (yes)
+						$needs_upgrading = 1;
+					}
+
 				}
-
 			}
+
+			// store the option so we don't have to check this everytime.
+			update_option( DLM_Constants::LU_OPTION_NEEDS_UPGRADING, $needs_upgrading );
 		}
 
-		return false;
+		// now convert to int and return if it's 1.
+		return ( 1 === absint( $needs_upgrading ) );
 	}
 
 }
