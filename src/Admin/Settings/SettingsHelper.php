bddae2dc9cf429ab3dedd9c72fd5e6294a028acd
barrykooij <b.kooij@gmail.com>
2018-10-02 22:58:33 +0200

    Adding SettingsHelper service. This will introduce a general way of fetching DLM options. Rest of project needs to be updated to use this method

---
+++ src/Admin/Settings/SettingsHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+class DLM_Settings_Helper {
+
+	/** @var array */
+	private $defaults;
+
+	/**
+	 * DLM_Settings_Helper constructor.
+	 */
+	public function __construct() {
+		$this->setup_defaults();
+	}
+
+	/**
+	 * Setup the defaults used in the get_option() method
+	 */
+	private function setup_defaults() {
+		$this->defaults = array(
+			'currency'              => 'USD',
+			'currency_pos'          => 'left',
+			'decimal_separator'     => '.',
+			'thousand_separator'    => ',',
+		);
+	}
+
+
+	/**
+	 * Get option
+	 *
+	 * @param $key
+	 *
+	 * @return mixed|null
+	 */
+	public function get_option( $key ) {
+		// get option from DB
+		return apply_filters( 'dlm_get_option', get_option( 'dlm_' . $key, ( isset( $this->defaults[ $key ] ) ? $this->defaults[ $key ] : null ) ), $key );
+	}
+
+}
