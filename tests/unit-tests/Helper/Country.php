04cf01e73d4cb024b12cf9460ab4002e57eab0ab
Barry Kooij <b.kooij@gmail.com>
2019-02-19 16:04:50 +0100

    Added Country test

---
+++ tests/unit-tests/Helper/Country.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Never5\DownloadMonitor\Tests\Helper;
+
+use Never5\DownloadMonitor\Shop\Services\Services;
+
+class Country extends \DLM_Unit_Test_Case {
+
+	/**
+	 * Test get_countries()
+	 */
+	public function test_get_countries() {
+		$countries = Services::get()->service( 'country' )->get_countries();
+
+		$this->assertTrue( ( count( $countries ) > 0 ) );
+
+		$this->assertTrue( in_array( "Netherlands", $countries ) );
+	}
+
+	/**
+	 * Test get_country_label_by_code()
+	 */
+	public function test_get_country_label_by_code() {
+		$country = Services::get()->service( 'country' )->get_country_label_by_code( "NL" );
+
+		$this->assertEquals( "Netherlands", $country );
+	}
+
+}
