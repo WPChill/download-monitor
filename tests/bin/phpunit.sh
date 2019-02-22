946e38cc0731c50844ac8476c1f1ac77a2e4d477
Barry Kooij <b.kooij@gmail.com>
2019-02-22 17:39:43 +0100

    Travis tweaks

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ ${TRAVIS_PHP_VERSION} == '7.1' ]]; then
+if [[ ${TRAVIS_PHP_VERSION} == '7.2' ]]; then
 	phpunit -c phpunit.xml --coverage-clover=coverage.clover
 else
 	phpunit -c phpunit.xml
