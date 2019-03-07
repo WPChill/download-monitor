0d66f585bd270d959d63b2d772355e910a72eaa6
Barry Kooij <b.kooij@gmail.com>
2019-03-07 19:34:24 +0100

    updated tests to work with new features

--- tests/unit-tests/Product/Product.php
+++ tests/unit-tests/Product/Product.php
@@ -1,8 +1,11 @@
 <?php
 
-use \Never5\DownloadMonitor\Shop\DownloadProduct;
+namespace Never5\DownloadMonitor\Tests\Product;
 
-class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
+use Never5\DownloadMonitor\Shop\Services\Services;
+use \Never5\DownloadMonitor\Shop\Product;
+
+class DLM_Test_DownloadProduct extends \DLM_Unit_Test_Case {
 
 	/**
 	 * test if setting and getting price works
@@ -10,13 +13,14 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 	public function test_price() {
 
 		// create new DownloadProduct
-		$download_product = new DownloadProduct\DownloadProduct();
+		/** @var Product\Product $product */
+		$product = Services::get()->service('product_factory')->make();
 
 		// set price to 100 cents
-		$download_product->set_price( 100 );
+		$product->set_price( 100 );
 
 		// check if the price is stored correctly
-		$this->assertEquals( 100, $download_product->get_price() );
+		$this->assertEquals( 100, $product->get_price() );
 	}
 
 	/**
@@ -24,7 +28,8 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 	 */
 	public function test_set_price_from_user_input() {
 
-		$download_product = new DownloadProduct\DownloadProduct();
+		/** @var Product\Product $product */
+		$product = Services::get()->service('product_factory')->make();
 
 		$o_ds = download_monitor()->service( 'settings' )->get_option( 'decimal_separator' );
 		$o_ts = download_monitor()->service( 'settings' )->get_option( 'thousand_separator' );
@@ -34,40 +39,40 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 		update_option( 'dlm_thousand_separator', ',' );
 
 		// set test with thousand and decimal separator
-		$download_product->set_price_from_user_input( "1,000.15" );
-		$this->assertEquals( 100015, $download_product->get_price() );
+		$product->set_price_from_user_input( "1,000.15" );
+		$this->assertEquals( 100015, $product->get_price() );
 
 		// set test with only thousand separator
-		$download_product->set_price_from_user_input( "1,337" );
-		$this->assertEquals( 133700, $download_product->get_price() );
+		$product->set_price_from_user_input( "1,337" );
+		$this->assertEquals( 133700, $product->get_price() );
 
 		// set test with only decimal separator
-		$download_product->set_price_from_user_input( "1234.56" );
-		$this->assertEquals( 123456, $download_product->get_price() );
+		$product->set_price_from_user_input( "1234.56" );
+		$this->assertEquals( 123456, $product->get_price() );
 
 		// set test with no separators
-		$download_product->set_price_from_user_input( "12" );
-		$this->assertEquals( 1200, $download_product->get_price() );
+		$product->set_price_from_user_input( "12" );
+		$this->assertEquals( 1200, $product->get_price() );
 
 		// reverse decimal and thousand sep
 		update_option( 'dlm_decimal_separator', ',' );
 		update_option( 'dlm_thousand_separator', '.' );
 
 		// set test with thousand and decimal separator
-		$download_product->set_price_from_user_input( "1.000,15" );
-		$this->assertEquals( 100015, $download_product->get_price() );
+		$product->set_price_from_user_input( "1.000,15" );
+		$this->assertEquals( 100015, $product->get_price() );
 
 		// set test with only thousand separator
-		$download_product->set_price_from_user_input( "1.337" );
-		$this->assertEquals( 133700, $download_product->get_price() );
+		$product->set_price_from_user_input( "1.337" );
+		$this->assertEquals( 133700, $product->get_price() );
 
 		// set test with only decimal separator
-		$download_product->set_price_from_user_input( "1234,56" );
-		$this->assertEquals( 123456, $download_product->get_price() );
+		$product->set_price_from_user_input( "1234,56" );
+		$this->assertEquals( 123456, $product->get_price() );
 
 		// set test with no separators
-		$download_product->set_price_from_user_input( "12" );
-		$this->assertEquals( 1200, $download_product->get_price() );
+		$product->set_price_from_user_input( "12" );
+		$this->assertEquals( 1200, $product->get_price() );
 
 		// reset separators
 		update_option( 'dlm_decimal_separator', $o_ds );
@@ -80,8 +85,9 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 	public function test_get_price_for_user_input() {
 
 		// create product with price
-		$download_product = new DownloadProduct\DownloadProduct();
-		$download_product->set_price( 123456 );
+		/** @var Product\Product $product */
+		$product = Services::get()->service('product_factory')->make();
+		$product->set_price( 123456 );
 
 		$o_ds = download_monitor()->service( 'settings' )->get_option( 'decimal_separator' );
 		$o_ts = download_monitor()->service( 'settings' )->get_option( 'thousand_separator' );
@@ -90,13 +96,13 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 		update_option( 'dlm_decimal_separator', '.' );
 		update_option( 'dlm_thousand_separator', ',' );
 
-		$this->assertEquals( "1,234.56", $download_product->get_price_for_user_input() );
+		$this->assertEquals( "1,234.56", $product->get_price_for_user_input() );
 
 		// reverse decimal and thousand sep
 		update_option( 'dlm_decimal_separator', ',' );
 		update_option( 'dlm_thousand_separator', '.' );
 
-		$this->assertEquals( "1.234,56", $download_product->get_price_for_user_input() );
+		$this->assertEquals( "1.234,56", $product->get_price_for_user_input() );
 
 		// reset separators
 		update_option( 'dlm_decimal_separator', $o_ds );
@@ -107,14 +113,15 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 	 * Test set_taxable and is_taxable
 	 */
 	public function test_taxable() {
-		$download_product = new DownloadProduct\DownloadProduct();
-		$download_product->set_taxable( false );
+		/** @var Product\Product $product */
+		$product = Services::get()->service('product_factory')->make();
+		$product->set_taxable( false );
 
-		$this->assertFalse( $download_product->is_taxable() );
+		$this->assertFalse( $product->is_taxable() );
 
-		$download_product->set_taxable( true );
+		$product->set_taxable( true );
 
-		$this->assertTrue( $download_product->is_taxable() );
+		$this->assertTrue( $product->is_taxable() );
 	}
 
 	/**
@@ -122,10 +129,11 @@ class DLM_Test_DownloadProduct extends DLM_Unit_Test_Case {
 	 */
 	public function test_tax_class() {
 
-		$download_product = new DownloadProduct\DownloadProduct();
-		$download_product->set_tax_class( "test_tax_class" );
+		/** @var Product\Product $product */
+		$product = Services::get()->service('product_factory')->make();
+		$product->set_tax_class( "test_tax_class" );
 
-		$this->assertEquals( "test_tax_class", $download_product->get_tax_class() );
+		$this->assertEquals( "test_tax_class", $product->get_tax_class() );
 	}
 
 }
