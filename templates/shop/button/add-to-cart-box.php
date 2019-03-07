a65cfd29c4ce32e83a74778298351f9d997a861f
Barry Kooij <b.kooij@gmail.com>
2019-03-07 12:51:26 +0100

    Shortcode now uses product

--- templates/shop/button/add-to-cart-box.php
+++ templates/shop/button/add-to-cart-box.php
@@ -7,20 +7,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
 
-/** @var \Never5\DownloadMonitor\Shop\DownloadProduct\DownloadProduct $download */
+/** @var \Never5\DownloadMonitor\Shop\Product\Product $product */
 /** @var string $atc_url */
 ?>
 <aside class="download-box">
 
-	<?php $download->the_image(); ?>
+	<?php echo $product->get_image(); ?>
 
-    <div class="download-count"><?php echo dlm_format_money( $download->get_price() ); ?></div>
+    <div class="download-count"><?php echo dlm_format_money( $product->get_price() ); ?></div>
 
     <div class="download-box-content">
 
-        <h1><?php $download->the_title(); ?></h1>
+        <h1><?php echo $product->get_title(); ?></h1>
 
-		<?php $download->the_excerpt(); ?>
+		<p><?php echo $product->get_excerpt(); ?></p>
 
         <a class="download-button" title="<?php _e( 'Purchase Now', 'download-monitor' ); ?>" href="<?php echo $atc_url; ?>"
            rel="nofollow">
