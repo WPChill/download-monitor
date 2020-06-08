198ebb0201501103ecb31a24c4dd22044f649ae9
Barry Kooij <barrykooij@users.noreply.github.com>
2020-06-08 20:31:08 +0200

    update to 4.4.3

--- download-monitor.php
+++ download-monitor.php
@@ -3,11 +3,11 @@
 	Plugin Name: Download Monitor
 	Plugin URI: https://www.download-monitor.com
 	Description: A full solution for managing and selling downloadable files, monitoring downloads and outputting download links and file information on your WordPress powered site.
-	Version: 4.4.2
+	Version: 4.4.3
 	Author: Never5
 	Author URI: https://www.never5.com
 	Requires at least: 3.8
-	Tested up to: 5.1.1
+	Tested up to: 5.4.1
 	Text Domain: download-monitor
 
 	License: GPL v3
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 } // Exit if accessed directly
 
 // Define DLM Version
-define( 'DLM_VERSION', '4.4.2' );
+define( 'DLM_VERSION', '4.4.3' );
 
 // Define DLM FILE
 define( 'DLM_PLUGIN_FILE', __FILE__ );
