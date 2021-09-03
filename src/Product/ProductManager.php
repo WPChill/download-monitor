d029854198673c1ea5d39defb1d05af8148efacd
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2021-09-03 12:17:09 +0300

    action for each extension; code removal;

--- src/Product/ProductManager.php
+++ src/Product/ProductManager.php
@@ -21,6 +21,13 @@ class DLM_Product_Manager {
 	 * @var DLM_Product_Error_Handler
 	 */
 	private $error_handler;
+
+	/**
+	 * Registered extensions
+	 *
+	 * @var
+	 */
+	private $registered_extensions = array();
 	
 	/**
 	 * Private constructor
@@ -66,20 +73,27 @@ class DLM_Product_Manager {
 	 */
 	public function load_extensions() {
 		// Load the registered extensions
-		$registered_extensions = apply_filters( 'dlm_extensions', array() );
+		$this->registered_extensions = apply_filters( 'dlm_extensions', array() );
 
 		// Check if we've got extensions
-		if ( count( $registered_extensions ) > 0 ) {
+		if ( count( $this->registered_extensions ) > 0 ) {
 
 			// Don't block local requests
 			add_filter( 'block_local_requests', '__return_false' );
 
 			// Load products
-			$this->load_products( $registered_extensions );
+			add_action( 'admin_init', array( $this, 'load_products_wrapper' ),999 );
 
 		}
 	}
 
+	/**
+	 *
+	 */
+	public function load_products_wrapper() {
+
+		$this->load_products( $this->registered_extensions );
+	}
 
 	/**
 	 * Load Products
@@ -106,10 +120,8 @@ class DLM_Product_Manager {
 				// Setup new Product
 				$product = new DLM_Product( $extension['file'], $extension['version'], $extension['name'] );
 
-				// Setup plugin actions and filters
-				add_action( 'pre_set_site_transient_update_plugins', array( $product, 'check_for_updates' ) );
-				add_filter( 'plugins_api', array( $product, 'plugins_api' ), 10, 3 );
-				add_action( 'after_plugin_row_' . $product->get_plugin_name(), array( $product, 'after_plugin_row' ), 10, 2 );
+				// Set action for each extension
+				do_action( 'dlm_extensions_action_' . $extension['file'], $extension );
 
 				// Add product to products property
 				$this->products[ $extension['file'] ] = $product;
@@ -125,6 +137,17 @@ class DLM_Product_Manager {
 	 * @return array<DLM_Product>
 	 */
 	public function get_products() {
+
 		return $this->products;
+	}
+
+	/**
+	 * Return extensions
+	 *
+	 * @return mixed
+	 */
+	public function get_extensions() {
+
+		return $this->registered_extensions;
 	}
 }
