f519367a235013f93c3846fb18ba7f6e70855fa2
Raiber Cristian <raibercristian@gmail.com>
2021-11-29 15:45:06 +0200

    cristi-reports-modifications

--- assets/js/reports/reports.js
+++ assets/js/reports/reports.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 jQuery( function ( $ ) {
 
 	// init chart blocks
@@ -146,6 +147,437 @@ DLM_Reports_Block_Chart.prototype.fetch = function () {
 DLM_Reports_Block_Chart.prototype.render = function () {
 	if ( this.data === null ) {
 		return;
+=======
+jQuery(function ($) {
+	new DLM_Reports();
+});
+
+class DLM_Reports {
+
+	/**
+	 * The constructor for our class
+	 */
+	constructor() {
+
+		this.chartContainer = document.getElementById('total_downloads_chart');
+		const ctx = this.chartContainer.getContext("2d");
+
+		/**
+		 * Gradient for the chart
+		 */
+		this.chartColors = {
+			purple: {
+				default: "rgba(149, 76, 233, 1)",
+				half: "rgba(149, 76, 233, 0.75)",
+				quarter: "rgba(149, 76, 233, 0.5)",
+				zero: "rgba(149, 76, 233, 0.05)"
+			},
+			blue: {
+				default: "rgba(56, 88, 233, 1)",
+				half: "rgba(56, 88, 233, 0.75)",
+				quarter: "rgba(56, 88, 233, 0.5)",
+				zero: "rgba(56, 88, 233, 0.05)"
+			},
+			indigo: {
+				default: "rgba(80, 102, 120, 1)",
+				quarter: "rgba(80, 102, 120, 0.5)"
+			}
+		};
+		this.chartGradient = ctx.createLinearGradient(0, 25, 0, 300);
+		this.chartGradient.addColorStop(0, this.chartColors.blue.half);
+		this.chartGradient.addColorStop(0.45, this.chartColors.blue.quarter);
+		this.chartGradient.addColorStop(1, this.chartColors.blue.zero);
+
+
+		this.datePickerContainer = document.getElementById('dlm-date-range-picker');
+		// We parse it so that we don't make any modifications to the actual data
+		// In case we will fetch data using js and the WP REST Api the this.reportsData will be an empty Object and we'll fetch data using fetchData() function
+		this.reportsData = ('undefined' !== typeof dlmReportsStats) ? JSON.parse(JSON.stringify(dlmReportsStats)) : {};
+		this.mostDownloaded = false;
+		this.stats = false;
+		this.chartType = 'day';
+		this.createDataOnDate(false, false);
+		this.datePicker = {
+			opened: false
+		};
+		this.init();
+	}
+
+	/**
+	 * Get all dates in set intervals
+	 * Used for chart data
+	 *
+	 * @param startDate
+	 * @param endDate
+	 * @returns {*[]}
+	 */
+	getDates(startDate, endDate) {
+
+		const dates = [];
+		let currentDate = startDate;
+
+		while (currentDate <= endDate) {
+
+			dates[this.createDateElement(currentDate)] = 0;
+			currentDate = this.getNextDay(currentDate);
+		}
+
+		return dates;
+	}
+
+	/**
+	 * Get all dates in set intervals
+	 * Used for chart data
+	 *
+	 * @param startDate
+	 * @param endDate
+	 * @returns {*[]}
+	 */
+	getMonths(days) {
+
+		const dates = [];
+
+		Object.keys(days).map(element => {
+
+			let subString = element.substring(0, 7);
+
+			if ('undefined' === typeof dates[subString]) {
+				dates[subString] = 0;
+			}
+		});
+
+		return dates;
+	}
+
+	/**
+	 * Get all dates in set intervals
+	 * Used for chart data
+	 *
+	 * @param startDate
+	 * @param endDate
+	 * @returns {*[]}
+	 */
+	getWeeks(days) {
+
+		const dates = [];
+
+		Object.keys(days).map(element => {
+			let week;
+
+			if (moment(element).date() > 15) {
+				week = element.substring(0, 7) + '-15';
+			} else {
+				week = element.substring(0, 7) + '-1';
+			}
+
+			if ('undefined' === typeof dates[week]) {
+				dates[week] = 0;
+			}
+		});
+
+		return dates;
+	}
+
+	/**
+	 * Get the next day
+	 *
+	 * @param currentDate
+	 * @returns {Date}
+	 */
+	getNextDay(currentDate) {
+
+		const date = new Date(currentDate);
+		date.setDate(currentDate.getDate() + 1);
+		return date;
+	}
+
+	/**
+	 * Requires a Date object and resturns a string
+	 * @param date
+	 * @returns {string}
+	 */
+	createDateElement(date) {
+		var MM = ((date.getMonth() + 1) < 10 ? '0' : '') +
+			(date.getMonth() + 1);
+
+		return date.getFullYear() + '-' + MM + '-' + date.getDate();
+	}
+
+	/**
+	 * Filter data to send to chart based on user input start & end date
+	 *
+	 * @param startDateInput
+	 * @param endDateInput
+	 * @param dataType
+	 * @returns {*}
+	 */
+	createDataOnDate(startDateInput, endDateInput) {
+
+		const instance = this;
+
+		let startDate,
+			endDate,
+			monthDiff,
+			chartDate,
+			dlmDownloads;
+		instance.reportsData = ('undefined' !== typeof dlmReportsStats) ? JSON.parse(JSON.stringify(dlmReportsStats)) : {};
+
+		if ('undefined' !== typeof startDateInput && startDateInput) {
+
+			startDate = instance.createDateElement(new Date(startDateInput));
+		} else {
+			// If there are no startDateInput it means it is the first load, so get last 30 days.
+			const lastMonth = new Date();
+			lastMonth.setDate(lastMonth.getDate() - 30);
+			startDate = instance.createDateElement(lastMonth);
+
+		}
+
+		if ('undefined' !== typeof endDateInput && endDateInput) {
+			endDate = instance.createDateElement(new Date(endDateInput));
+		} else {
+
+			// If there is no endDateInput we'll set the endDate to tomorrow so that we can include today in our reports also.
+			// Seems like this is how the datepicker works.
+			const tomorrow = new Date();
+			tomorrow.setDate(tomorrow.getDate() + 1);
+			endDate = instance.createDateElement(tomorrow);
+		}
+
+		monthDiff = moment(endDate, 'YYYY-MM-DD').month() - moment(startDate, 'YYYY-MM-DD').month();
+
+		if (moment(endDate, 'YYYY-MM-DD').year() !== moment(startDate, 'YYYY-MM-DD').year() || monthDiff > 6) {
+			instance.chartType = 'month';
+		} else if (monthDiff >= 1) {
+
+			instance.chartType = 'weeks';
+
+			if (1 === monthDiff && moment(endDate, 'YYYY-MM-DD').date() <= moment(startDate, 'YYYY-MM-DD').date()) {
+				instance.chartType = 'day';
+			}
+
+		} else {
+			instance.chartType = 'day';
+		}
+
+		// Get all dates from the startDate to the endDate
+		let dayDownloads = instance.getDates(new Date(startDate), new Date(endDate));
+		// Get selected months
+		let monthDownloads = instance.getMonths(dayDownloads);
+		// Get selected dates in 2 weeks grouping
+		let weeksDownloads = instance.getWeeks(dayDownloads);
+
+		Object.values(instance.reportsData).forEach((day, index) => {
+
+			const downloads = JSON.parse(day.download_ids);
+
+			if ('undefined' !== typeof dayDownloads[day.date]) {
+
+				switch (instance.chartType) {
+					case 'month':
+
+						chartDate = day.date.substring(0, 7);
+						Object.values(downloads).forEach((item, index) => {
+
+							monthDownloads[chartDate] = ('undefined' !== typeof monthDownloads[chartDate]) ? monthDownloads[chartDate] + item.downloads : item.downloads;
+
+						});
+
+						dlmDownloads = monthDownloads;
+						break;
+					case 'weeks':
+
+						if (moment(day.date).date() > 15) {
+							chartDate = day.date.substring(0, 7) + '-15';
+						} else {
+							chartDate = day.date.substring(0, 7) + '-1';
+						}
+
+						Object.values(downloads).forEach((item, index) => {
+
+							weeksDownloads[chartDate] = ('undefined' !== typeof weeksDownloads[chartDate]) ? weeksDownloads[chartDate] + item.downloads : item.downloads;
+
+						});
+
+						dlmDownloads = weeksDownloads;
+						break;
+					case 'day':
+						Object.values(downloads).forEach((item, index) => {
+
+							dayDownloads[day.date] = dayDownloads[day.date] + item.downloads;
+
+						});
+
+						dlmDownloads = dayDownloads;
+						break;
+				}
+
+			} else {
+				delete instance.reportsData[index];
+			}
+
+		});
+
+		// Get number of days, used in summary for daily average downloads
+		const daysLength = Object.keys(dayDownloads).length;
+
+		// Find the start of the donwloads object
+		let start = Object.keys(dayDownloads).findIndex((element) => {
+			return startDate === element;
+		});
+
+		// Find the end of the downloads object
+		let end = Object.keys(dayDownloads).findIndex((element) => {
+			return endDate === element;
+		});
+
+		if (-1 === start && -1 === end) {
+
+			instance.stats = {
+				chartStats: Object.assign({}, dlmDownloads),
+				summaryStats: false,
+				daysLength: daysLength
+			};
+			return;
+		}
+
+		if (-1 === start) {
+			start = 0;
+		}
+
+		if (-1 === end) {
+			end = daysLength;
+		}
+
+		instance.stats = {
+			chartStats: Object.assign({}, dlmDownloads),
+			summaryStats: instance.reportsData,
+			daysLength: daysLength
+		};
+
+	}
+
+	/**
+	 * Let's create our chart.
+	 * 
+	 * @param {*} data 
+	 * @param {*} chartId 
+	 */
+	dlmCreateChart(data, chartId) {
+
+		if (data && chartId) {
+
+			const instance = this;
+
+			const chart = Chart.getChart('total_downloads_chart');
+
+			if ('undefined' !== typeof chart) {
+				chart.destroy();
+			}
+
+			// Set here the dataSets
+			const dataSets = [{
+				label: 'Downloads',
+				//color: '#27ae60',
+				color: 'a299ff',
+				data: data,
+				type: 'line',
+				fill: true,
+				backgroundColor: instance.chartGradient,
+				pointBackgroundColor: instance.chartColors.blue.default,
+				pointHoverBackgroundColor: '#fff',
+				borderColor: instance.chartColors.blue.default,
+				pointBorderWidth: 6,
+				lineTension: 0.2,
+				borderWidth: 2,
+				pointRadius: 3,
+				elements: {
+					line: {
+						borderColor: '#2ecc71',
+						borderWidth: 2,
+					},
+					point: {
+						radius: 4,
+						hoverRadius: 8,
+						pointStyle: 'circle'
+					}
+				},
+			}, ];
+
+			instance.chart = new Chart(chartId, {
+				title: "",
+				data: {
+					datasets: dataSets
+				},
+				height: 450,
+				is_series: 1,
+				options: {
+					aspectRatio: 5,
+					animation: false,
+					scales: {
+						x: {
+							grid: {
+								display: false,
+							},
+							ticks: {
+								callback: (val) => {
+									return ('undefined' !== typeof instance.chartType && 'month' === instance.chartType) ? moment(Object.keys(data)[val]).format("D MMM YY") : moment(Object.keys(data)[val]).format("D MMM");
+								}
+							},
+						},
+						y: {
+							ticks: {
+								align: 'center',
+								display: true,
+								}
+							},
+					},
+					normalized: true,
+					parsing: {
+						xAxisKey: 'x',
+						yAxisKey: 'y'
+					},
+					plugins: {
+						legend: {
+							display: false,
+						},
+						tooltip: {
+							// Should be deleted if we remain on external tooltip
+							/* backgroundColor: '#fff',
+							titleColor: instance.chartColors.blue.default,
+							yAlign: "bottom",
+							xAlign: "bottom",
+							titleAlign: 'center',
+							titleFont: {
+								weight: 'bold',
+								size: 18
+							},
+							padding: {
+								left: 15,
+								right: 15,
+								top: 30,
+								bottom: 30,
+							},
+							cornerRadius: 8,
+							borderColor: instance.chartColors.blue.default,
+							borderWidth: 1,
+							displayColors: false,
+							bodyColor: '#000',
+							callbacks: {
+								title: context => context[0].formattedValue,
+								label: context => '',
+								beforeLabel: context => 'Downloads',
+								afterLabel: context => ('undefined' !== instance.chartType && 'month' === instance.chartType) ? moment(context.label).format("MMMM, YYYY") : moment(context.label).format("dddd, MMMM Do YYYY"),
+								labelTextColor: context => instance.chartColors.blue.half,
+							}, */
+							enabled: false,
+							external: instance.externalTooltipHandler.bind(instance,this),
+						},
+					},
+				},
+			});
+		}
+>>>>>>> Stashed changes
 	}
 	var chartId = document.getElementById('total_downloads_chart');
 	this.chart = new Chart( chartId, {
