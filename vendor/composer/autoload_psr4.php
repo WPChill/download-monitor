7c2705e407154308e7b1fbc1e10badeb22ea7821
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2022-03-01 15:52:24 +0200

    autoload

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'WPChill\\DownloadMonitor\\' => array($baseDir . '/src'),
+    'Never5\\DownloadMonitor\\' => array($baseDir . '/src'),
 );
