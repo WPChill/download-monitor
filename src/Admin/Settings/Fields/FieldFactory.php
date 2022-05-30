44d11b529d8096ba5dede40a2dfef56599ae35a5
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2022-05-30 13:47:02 +0300

    add missing field types

--- src/Admin/Settings/Fields/FieldFactory.php
+++ src/Admin/Settings/Fields/FieldFactory.php
@@ -57,6 +57,9 @@ class DLM_Admin_Fields_Field_Factory {
 			case 'htaccess_status':
 				$field = new DLM_Admin_Fields_Field_HtaccessStatus( $option );
 				break;
+			case 'group':
+				$field = new DLM_Admin_Fields_Field_Accordion( $option['name'], $option['options'], $option['title'] );
+				break;
 			default:
 				/**
 				 * do_filter: dlm_setting_field_$type: (null) $field, (array) $option, (String) $value, (String) $placeholder
