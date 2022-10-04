9dd5b6891d1e38757036591e8cf43aae8fa3bde5
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2022-10-04 14:24:00 +0300

    [ #1054 ] - UI design

--- src/Admin/Reports/components/php-components/top-downloads-footer.php
+++ src/Admin/Reports/components/php-components/top-downloads-footer.php
@@ -6,7 +6,7 @@
 </table>
 <div class="total_downloads_table_footer">
 	<div class="downloads-block-navigation">
-		<?php esc_html_e( 'Page:', 'download-monitor' ) ?><input type="number" value="1" class="dlm-reports-current-page"> <?php esc_html_e( 'of', 'download-monitor' ) ?> <span class="dlm-reports-total-pages">1</span>
+		<?php esc_html_e( 'Page:', 'download-monitor' ) ?> <input type="number" value="1" class="dlm-reports-current-page"> <span><?php esc_html_e( 'of', 'download-monitor' ) ?></span> <span class="dlm-reports-total-pages">1</span>
 		<button class='dashicons dashicons-arrow-left-alt2 hidden' disabled='disabled' data-limit="15"
 		        title='Previous 15 downloads'></button>
 		<button class='dashicons dashicons-arrow-right-alt2 hidden' data-action='load-more' data-offset="1" data-limit="15"
