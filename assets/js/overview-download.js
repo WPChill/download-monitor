7dfdea2defb70f71a80a2c4725ce2b45845dee3c
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-01-12 17:29:41 +0200

    [ #1072 ] - fix copy shortcode

--- assets/js/overview-download.js
+++ assets/js/overview-download.js
@@ -45,7 +45,15 @@
         dlm_shortcode.trigger("focus");
         dlm_shortcode.trigger("select");
         document.execCommand('copy');
-        navigator.clipboard.writeText(dlm_shortcode.val());
+
+        if ( 'undefined' !== typeof navigator.clipboard ) {
+            navigator.clipboard.writeText(dlm_shortcode.val());
+        }
+
+        if ( 'undefined' !== typeof window.ClipboardJS  ) {
+            window.ClipboardJS.copy(dlm_shortcode.val());
+        }
+
         $(this).next('span').text($(this).data('item') + ' copied');
         $('.copy-dlm-button').not($(this)).parent().find('span').text('');
         dlm_shortcode.trigger("blur");
