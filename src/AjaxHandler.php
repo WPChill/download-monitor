3d90d636dfebf47c66095a011885dc1081a58b33
TeoAlex <teoalexsin@gmail.com>
2023-09-13 11:56:05 +0300

    tailwind2

--- src/AjaxHandler.php
+++ src/AjaxHandler.php
@@ -386,6 +386,39 @@ class DLM_Ajax_Handler {
 					</div>
 				</div>			
 			</div>';
+
+
+			$modal_template = '<div id="dlm-no-access-modal" >
+			<div class="dlm-no-access-modal-overlay">
+
+			</div>
+			<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+				<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+				<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+				<div class="dlm-no-access-modal-window flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+					<div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+					<div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+						<div class="sm:flex sm:items-start">
+						<div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+							<svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+							</svg>
+						</div>
+						<div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+							<h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">' . esc_html( $title[ $restriction_type ] ) . '</h3>
+							<div class="mt-2">
+							<p class="text-sm text-gray-500">' . $content . '</p>
+							</div>
+						</div>
+						</div>
+					</div>
+					<div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+						<button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">' . esc_html__( 'Close', 'download-monitor' ) . '</button>
+					</div>
+					</div>
+				</div>
+				</div>
+				</div></div>';
 		// Content and variables escaped above.
 		// $content variable escaped from extensions as it may include inputs or other HTML elements.
 		echo $modal_template; //phpcs:ignore
