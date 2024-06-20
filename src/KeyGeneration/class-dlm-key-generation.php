182b94dfdd9cee8b88eb547a691929628d3d0732
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2024-06-20 10:54:08 +0300

    [ #1215 ] - add filter to enable the api keys menu entry

--- src/KeyGeneration/class-dlm-key-generation.php
+++ src/KeyGeneration/class-dlm-key-generation.php
@@ -70,15 +70,27 @@ class DLM_Key_Generation {
 	 * @since 5.0.0
 	 */
 	public function add_admin_menu() {
-		// Add submenu page.
-		add_submenu_page(
-			'edit.php?post_type=dlm_download',
-			__( 'API Keys', 'download-monitor' ),
-			__( 'API Keys', 'download-monitor' ),
-			'manage_options',
-			'dlm-api-keys',
-			array( $this, 'render_api_keys_page' )
-		);
+		/**
+		 * Filter to enable API keys menu entry
+		 *
+		 * @hook  dlm_enable_api_keys
+		 *
+		 * @param  bool  $enable_api_keys  True to enable API keys, false to disable.
+		 *
+		 * @since 5.0.0
+		 *
+		 */
+		if ( apply_filters( 'dlm_enable_api_keys', false ) ) {
+			// Add submenu page.
+			add_submenu_page(
+				'edit.php?post_type=dlm_download',
+				__( 'API Keys', 'download-monitor' ),
+				__( 'API Keys', 'download-monitor' ),
+				'manage_options',
+				'dlm-api-keys',
+				array( $this, 'render_api_keys_page' )
+			);
+		}
 	}
 
 	/**
@@ -99,7 +111,8 @@ class DLM_Key_Generation {
 					echo esc_attr( $current_user->data->ID ); ?>"> <?php
 						echo esc_html( $current_user->data->display_name . '(' . $current_user->data->user_email . ')' ); ?> </option>
 				</select>
-				<button class="dlm-keygen-generate button button-secondary"><?php echo esc_html__( 'Generate API Key', 'download-monitor' ); ?></button>
+				<button class="dlm-keygen-generate button button-secondary"><?php
+					echo esc_html__( 'Generate API Key', 'download-monitor' ); ?></button>
 			</div>
 			<?php
 			// Add your code here.
