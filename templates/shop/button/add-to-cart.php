0491759814bccd027f72d26f7af6cda756c36f08
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2024-08-07 11:33:16 +0300

    [ #1505 ] - extra button fix

--- templates/shop/button/add-to-cart.php
+++ templates/shop/button/add-to-cart.php
@@ -14,21 +14,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 /** @var \WPChill\DownloadMonitor\Shop\Product\Product $product */
 /** @var string $atc_url */
 ?>
-<p>
-	<a class="aligncenter download-button" href="<?php
-	echo esc_url( $atc_url ); ?>" rel="nofollow">
-		<?php
-		printf( esc_html__( 'Purchase &ldquo;%s&rdquo;', 'download-monitor' ), esc_html( $product->get_title() ) ); ?>
-		<small><?php
-			echo esc_html( dlm_format_money( $product->get_price() ) ); ?>
-			<?php
-			$excerpt = $product->get_excerpt();
-			if ( ! empty( $excerpt ) ) {
-			?>
-			- <?php
-			$product->the_excerpt(); ?></small>
+<a class="aligncenter download-button" href="<?php
+echo esc_url( $atc_url ); ?>" rel="nofollow">
 	<?php
-	}
-	?>
-	</a>
-</p>
+	printf( esc_html__( 'Purchase &ldquo;%s&rdquo;', 'download-monitor' ), esc_html( $product->get_title() ) ); ?>
+	<small><?php
+		echo esc_html( dlm_format_money( $product->get_price() ) ); ?>
+		<?php
+		$excerpt = $product->get_excerpt();
+		if ( ! empty( $excerpt ) ) {
+		?>
+		- <?php
+		$product->the_excerpt(); ?></small>
+<?php
+}
+?>
+</a>
