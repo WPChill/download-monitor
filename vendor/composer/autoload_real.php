69c6edac87ee5c99e679c930331b1939503c3d50
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2024-08-23 08:45:56 +0300

    5.0.3 update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,18 +31,6 @@ class ComposerAutoloaderInit80ce4473100edd20fd6c17775a76ce9a
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit80ce4473100edd20fd6c17775a76ce9a::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
