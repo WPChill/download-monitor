a0393b14de185b1bf37800428c538817df35426b
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2024-10-21 10:58:36 +0300

    modal script fix

--- assets/js/modal-upsells.js
+++ assets/js/modal-upsells.js
@@ -1,90 +1,88 @@
-jQuery(function ($) {
+/**
+ * Class to handle modal upsells
+ *
+ * @since 5.0.13
+ */
+class dlmModalUpsells {
+  upsells = [];
+
   /**
-   * Class to handle modal upsells
+   * Constructor
    *
    * @since 5.0.13
    */
-  class dlmModalUpsells {
-    upsells = [];
+  constructor() {
+    this.init();
+  }
 
-    /**
-     * Constructor
-     *
-     * @since 5.0.13
-     */
-    constructor() {
-      this.init();
-    }
+  /**
+   * Initialize
+   *
+   * @since 5.0.13
+   */
+  init() {
+    const instance = this;
+    instance.upsells = dlmModalUpsellsVars.upsells;
+    instance.bindEvents();
+  }
 
-    /**
-     * Initialize
-     *
-     * @since 5.0.13
-     */
-    init() {
-      const instance = this;
-      instance.upsells = dlmModalUpsellsVars.upsells;
-      instance.bindEvents();
-    }
-
-    /**
-     * Bind events
-     *
-     * @since 5.0.13
-     */
-    bindEvents() {
-      const instance = this;
-      if ("0" !== instance.upsells.length) {
-        // Bind click event to open modal
-        for (let key in instance.upsells) {
-          $("body").on("click", `a[href='${key}_upsell_modal']`, function (e) {
+  /**
+   * Bind events
+   *
+   * @since 5.0.13
+   */
+  bindEvents() {
+    const instance = this;
+    if ("0" !== instance.upsells.length) {
+      // Bind click event to open modal
+      for (let key in instance.upsells) {
+        jQuery("body").on("click", `a[href='${key}_upsell_modal']`, function (e) {
+          e.preventDefault();
+          instance.openModal(key);
+        });
+        // Bind click event to close modal
+        jQuery("body").on(
+          "click",
+          `.dlm-modal__overlay.${key}, .dlm-modal__overlay.${key} .dlm-modal__dismiss`,
+          function (e) {
             e.preventDefault();
-            instance.openModal(key);
-          });
-          // Bind click event to close modal
-          $("body").on(
-            "click",
-            `.dlm-modal__overlay.${key}, .dlm-modal__overlay.${key} .dlm-modal__dismiss`,
-            function (e) {
-              e.preventDefault();
-              instance.closeModal(key);
-            }
-          );
-        }
+            instance.closeModal(key);
+          }
+        );
       }
-    }
-
-    /**
-     * Open modal
-     *
-     * @since 5.0.13
-     */
-    openModal(upsell) {
-      const data = {
-        action: "dlm_upsell_modal",
-        security: dlmModalUpsellsVars.security,
-        upsell: upsell,
-      };
-      $.post(ajaxurl, data, function (response) {
-        const $body = $("body");
-        $body.addClass("modal-open");
-        $body.append(response);
-      });
-    }
-
-    /**
-     * Close modal
-     *
-     * @since 5.0.13
-     */
-    closeModal(upsell) {
-      $(`.dlm-modal__overlay.${upsell}`).remove();
-      $("body").removeClass("modal-open");
     }
   }
 
-  // Load the class when window loaded
-  $(document).ready(function () {
-    new dlmModalUpsells();
-  });
+  /**
+   * Open modal
+   *
+   * @since 5.0.13
+   */
+  openModal(upsell) {
+    const data = {
+      action: "dlm_upsell_modal",
+      security: dlmModalUpsellsVars.security,
+      upsell: upsell,
+    };
+    jQuery.post(ajaxurl, data, function (response) {
+      const $body = jQuery("body");
+      $body.addClass("modal-open");
+      $body.append(response);
+    });
+  }
+
+  /**
+   * Close modal
+   *
+   * @since 5.0.13
+   */
+  closeModal(upsell) {
+    jQuery(`.dlm-modal__overlay.${upsell}`).remove();
+    jQuery("body").removeClass("modal-open");
+  }
+}
+
+// Load the class when window loaded
+jQuery(document).ready(function () {
+  new dlmModalUpsells();
 });
