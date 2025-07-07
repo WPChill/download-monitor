1db0029c50a5c88c73c061c79a3aa6cbff4da96c
TeoAlex <teoalexsin@gmail.com>
2025-07-07 13:36:11 +0300

    [ #1657 ] Overview Tab - card + added slot & simplified check

--- assets/apps/reports/components/OverviewCards.jsx
+++ assets/apps/reports/components/OverviewCards.jsx
@@ -11,36 +11,55 @@ export default function OverviewCards() {
 		isLoading,
 		error,
 	} = useGetCards( state.periods );
-	//return `start: ${ state.periods.start } end: ${ state.periods.end }`;
 
 	return (
 		<div className={ styles.dlmReportsCardsWrapper } >
 			<div className={ styles.dlmReportsCard }>
-				<h3 className={ styles.dlmReportsCardTitle } >{ __( 'Total Downloads', 'download-monitor' ) }</h3>
-				{ isLoading
-					? ( <p className={ styles.dlmReportsCardValue } ><Spinner /></p> )
-					: ( <p className={ styles.dlmReportsCardValue } >{ cards?.total || 0 }</p> ) }
+				<h3 className={ styles.dlmReportsCardTitle }>
+					{ __( 'Total Downloads', 'download-monitor' ) }
+				</h3>
+				<p className={ styles.dlmReportsCardValue }>
+					{ isLoading ? <Spinner /> : cards?.total || 0 }
+				</p>
 				<span id="dlm-card-slot-total" />
 				<Slot name="dlm.card.total.after" containerId="dlm-card-slot-total" cards={ cards } />
 			</div>
+
 			<div className={ styles.dlmReportsCard }>
-				<h3 className={ styles.dlmReportsCardTitle } >{ __( 'Today Downloads', 'download-monitor' ) }</h3>
-				{ isLoading
-					? ( <p className={ styles.dlmReportsCardValue } ><Spinner /></p> )
-					: ( <p className={ styles.dlmReportsCardValue } >{ cards?.today || 0 }</p> ) }
+				<h3 className={ styles.dlmReportsCardTitle }>
+					{ __( 'Today Downloads', 'download-monitor' ) }
+				</h3>
+				<p className={ styles.dlmReportsCardValue }>
+					{ isLoading ? <Spinner /> : cards?.today || 0 }
+				</p>
+				<span id="dlm-card-slot-today" />
+				<Slot name="dlm.card.today.after" containerId="dlm-card-slot-today" cards={ cards } />
 			</div>
+
 			<div className={ styles.dlmReportsCard }>
-				<h3 className={ styles.dlmReportsCardTitle } >{ __( 'Most popular download', 'download-monitor' ) }</h3>
-				{ isLoading
-					? ( <p className={ styles.dlmReportsCardValue } ><Spinner /></p> )
-					: ( <p className={ styles.dlmReportsCardValue } >{ cards?.most_popular?.title || __( 'No Title', 'download-monitor' ) }</p> ) }
+				<h3 className={ styles.dlmReportsCardTitle }>
+					{ __( 'Most popular download', 'download-monitor' ) }
+				</h3>
+				<p className={ styles.dlmReportsCardValue }>
+					{ isLoading ? <Spinner /> : cards?.most_popular?.title || __( 'No Title', 'download-monitor' ) }
+				</p>
+				<span id="dlm-card-slot-popular" />
+				<Slot name="dlm.card.popular.after" containerId="dlm-card-slot-popular" cards={ cards } />
 			</div>
+
 			<div className={ styles.dlmReportsCard }>
-				<h3 className={ styles.dlmReportsCardTitle } >{ __( 'Daily average downloads', 'download-monitor' ) }</h3>
-				{ isLoading
-					? ( <p className={ styles.dlmReportsCardValue } ><Spinner /></p> )
-					: ( <p className={ styles.dlmReportsCardValue } >{ cards?.average || 0 }</p> ) }
+				<h3 className={ styles.dlmReportsCardTitle }>
+					{ __( 'Daily average downloads', 'download-monitor' ) }
+				</h3>
+				<p className={ styles.dlmReportsCardValue }>
+					{ isLoading ? <Spinner /> : cards?.average || 0 }
+				</p>
+				<span id="dlm-card-slot-average" />
+				<Slot name="dlm.card.average.after" containerId="dlm-card-slot-average" cards={ cards } />
 			</div>
+
+			<span id="dlm-cards-slot-after" />
+			<Slot name="dlm.cards.after" containerId="dlm-cards-slot-after" cards={ cards } />
 		</div>
 	);
 }
