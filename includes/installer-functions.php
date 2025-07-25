f765c497efe62e28cad11aa0559d0144c60b43fd
TeoAlex <teoalexsin@gmail.com>
2025-07-25 16:43:41 +0300

    release 5.0.30

--- includes/installer-functions.php
+++ includes/installer-functions.php
@@ -23,7 +23,7 @@ function _download_monitor_install( $network_wide = false ) {
 
 	// check if.
 	if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
-		require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		require_once ABSPATH . '/wp-admin/includes/plugin.php';
 	}
 
 	// check if it's multisite.
@@ -51,6 +51,9 @@ function _download_monitor_install( $network_wide = false ) {
 		// no multisite so do normal install.
 		$installer->install();
 	}
+
+	// sets the default language of dlm_downloads post types
+	dlm_set_wpml_language_default();
 }
 
 /**
@@ -199,5 +202,58 @@ function dlm_handle_wpml_translation_ajx() {
 
 	if ( $string === $endpoint_option ) {
 		set_transient( 'dlm_download_endpoints_rewrite', true, HOUR_IN_SECONDS );
+	}
+}
+
+function dlm_set_wpml_language_default() {
+	if ( ! defined( 'ICL_SITEPRESS_VERSION' ) || ! function_exists( 'wpml_get_default_language' ) || ! class_exists( 'WP_Query' ) ) {
+		return;
+	}
+
+	global $sitepress;
+
+	if ( ! method_exists( $sitepress, 'set_element_language_details' ) ) {
+		return;
+	}
+
+	$default_lang = apply_filters( 'wpml_default_language', null );
+	if ( ! $default_lang ) {
+		return;
+	}
+
+	$args = array(
+		'post_type'      => 'dlm_download',
+		'post_status'    => 'any',
+		'posts_per_page' => -1,
+		'fields'         => 'ids',
+		'meta_query'     => array(
+			array(
+				'key'     => '_icl_lang_duplicate',
+				'compare' => 'NOT EXISTS',
+			),
+		),
+	);
+
+	$downloads = get_posts( $args );
+
+	foreach ( $downloads as $post_id ) {
+		$has_language = apply_filters(
+			'wpml_element_language_code',
+			null,
+			array(
+				'element_id'   => $post_id,
+				'element_type' => 'post_dlm_download',
+			)
+		);
+
+		if ( ! $has_language ) {
+			$sitepress->set_element_language_details(
+				$post_id,
+				'post_dlm_download',
+				null,
+				$default_lang,
+				null
+			);
+		}
 	}
 }
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpchill, silkalns, barrykooij, mikejolley
 Tags: download manager, file manager, digital store, ecommerce, password protection  
 Requires at least: 5.5  
 Tested up to: 6.8  
-Stable tag: 5.0.29
+Stable tag: 5.0.30
 License: GPLv3  
 Requires PHP: 7.6  
 
@@ -114,6 +114,9 @@ Admin hits are not counted, log out and try again!
 4. The quick add panel can be opened via a link about the post editor. This lets you quickly add a file and insert it into a post.
 
 == Changelog ==
+= 5.0.30 - 25.07.2025 =
+Fixed: Downloads without a default WPML language were not showing in the admin table.
+
 = 5.0.29 - 23.07.2025 =
 Fixed: Compatibility with Admin Columns plugin.
 
